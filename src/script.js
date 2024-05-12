54dc0de0a5998c808335b8bbcbd1a9a19c6404b2
1967sammy <samueladdofoli8@gmail.com>
2024-05-12 16:46:10 +0000

    first commit

---
+++ src/script.js
@@ -0,0 +1,59 @@
+document.getElementById("cart").addEventListener("click", () => {
+    document.getElementById("modal").style.display = "flex";
+     
+  });
+  
+  document.getElementById("cart2").addEventListener("click", () => {
+    document.getElementById("modal").style.display = "flex";
+    
+  });
+  
+  document.getElementById("cart3").addEventListener("click", () => {
+    document.getElementById("modal").style.display = "flex";
+    
+  });
+  document.getElementById("cart4").addEventListener("click", () => {
+    document.getElementById("modal").style.display = "flex";
+     
+  });
+  
+  document.getElementById("cart5").addEventListener("click", () => {
+    document.getElementById("modal").style.display = "flex";
+     
+  });
+  
+  document.getElementById("cart6").addEventListener("click", () => {
+    document.getElementById("modal").style.display = "flex";
+     
+  });
+  document.getElementById("cart7").addEventListener("click", () => {
+    document.getElementById("modal").style.display = "flex";
+     
+  });
+  document.getElementById("cart8").addEventListener("click", () => {
+    document.getElementById("modal").style.display = "flex";
+     
+  });
+  
+  document.getElementById("cart9").addEventListener("click", () => {
+    document.getElementById("modal").style.display = "flex";
+    
+  });
+  document.getElementById("cart10").addEventListener("click", () => {
+    document.getElementById("modal").style.display = "flex";
+    
+  });
+  document.getElementById("cart11").addEventListener("click", () => {
+    document.getElementById("modal").style.display = "flex";
+    
+  });
+  document.getElementById("cart12").addEventListener("click", () => {
+    document.getElementById("modal").style.display = "flex";
+     
+  });
+  
+  
+  document.getElementById("close").addEventListener("click", (e) => {
+    e.preventDefault();
+    document.getElementById("modal").style.display = "none";
+  });
